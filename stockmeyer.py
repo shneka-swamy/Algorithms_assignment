0910f94655337bf0d0bbe3a38f5414fc295c6a74
shneka-swamy <42727169+shneka-swamy@users.noreply.github.com>
2018-09-22 22:26:46 -0500

    stockmeyer_new file

--- stockmeyer.py
+++ stockmeyer.py
@@ -1,3 +1,4 @@
+#This is to form the left and right subarray in the increasing order of width
 def make_list(sub_array, h_w, index):
 
     if (h_w[index][0] > h_w[index][1]):
@@ -11,7 +12,8 @@ def make_list(sub_array, h_w, index):
     else:
         sub_array.append(h_w[index])
 
-def join_vertical(temp, Left, Right, val):
+#This is to join the blocks based on stockmeyers algorithm (when Vertical cut is encountered)
+def join_vertical(temp , Left, Right, val):
     inter = []
     k = len(Left)
     m = len(Right)
@@ -29,7 +31,8 @@ def join_vertical(temp, Left, Right, val):
             j = j+1
     temp[val] = inter
 
-def join_horizontal(temp, Left, Right, val):
+#This is to join the blocks based on stockmeyers algorithm (when Horizontal cut is encountered)
+def join_horizontal(temp , Left, Right,val):
     inter = []
     i = len(Left) - 1
     j = len(Right) - 1
@@ -45,17 +48,44 @@ def join_horizontal(temp, Left, Right, val):
             j = j-1
     temp[val] = inter
 
+
+def topdown(expr, temp, h_w, t_d):
+    n = len(expr)
+    L = []
+    R = []
+    
+    for i in range(0,n,3):
+        L[:] = []
+        R[:] = []
+
+        if(type(expr[i]) == int):
+            inter = expr[i] - 1
+            L.append(h_w[inter])
+        else:
+            L = temp[int(expr[i][1])]
+
+        if(type(expr[i+1]) == int):
+            inter = expr[i+1]-1
+            R.append(h_w[inter])
+        else:
+            R = temp[int(expr[i+1][1])]
+
+        print(L)
+        print(R)
+        
 def optimal_solution (p_e,h_w):
 
     val = 0
-    n = len(p_e)
-    m = (n-1)/2
+    n = len(p_e) 
+    m = (n-1)/2 #finds the number of internal nodes
     Left = []
     Right = []
     stack = []
-    temp = [[]]*m
+    index = (m*3) - 1 # this is used for forming the array required for top-down tree approach
+    expr = [0]*(m*3) # Top- down array formed
+    temp = [[]]*m # This a list of list for storing the list at internal nodes.
     
-
+    # Bottom- up part of the code
     for i in range(0,n):
         Left[:] = []
         Right[:] = []
@@ -65,33 +95,61 @@ def optimal_solution (p_e,h_w):
             ri = stack.pop()
             le = stack.pop()
 
-            if(type(le) == int):
-                make_list(Left, h_w, le-1)
-                print(Left)
-            else:
-                Left = temp[int(le[1])]
-                print(Left)
-                                
-            if(type(ri)== int):
-                make_list(Right, h_w, ri-1)
-                print(Right)
-            else:
-                Right = temp[int(ri[1])]
-                print(Right)
+            # To create the list for top - down traversal
+            expr[index] = p_e[i]
+            index -= 1
+            expr[index] = ri
+            index -= 1
+            expr[index] = le
+            index-= 1
 
+
+            inter_vert = []
+            inter_hori = []
+
+            # when the node popped out is integer then the node encountered is a leaf node
+            # otherwise, the node is an internan node.
+            # All the internal node with V or H are converted to D0, D1, D2, ... Dn for the sake of calculation)
+                
             if(p_e[i]=='v'or p_e[i]=='V'):
-                join_vertical(temp,Left,Right,val)
+                if(type(le) == int and type(ri) == int):
+                    make_list(Left, h_w, le-1)
+                    make_list(Right, h_w, ri-1)
+                    join_vertical(temp,Left,Right,val)
+                elif(type(le) != int and type(ri) == int):
+                    make_list(Right, h_w, ri-1)
+                    join_vertical(temp,temp[int(le[1])],Right,val)
+                elif(type(le) == int and type(ri)!= int):
+                    make_list(Left, h_w, le-1)
+                    join_vertical(temp,Left,temp[int(ri[1])],val)
+                else:
+                    join_vertical(temp,temp[int(le[1])],temp[int(ri[1])],val)
+
                 print(temp)
-            else:
-                join_horizontal(temp,Left,Right,val)
+                
+            elif(p_e[i]=='h' or p_e[i] == 'H'):
+                if(type(le) == int and type(ri) == int):
+                    make_list(Left, h_w, le-1)
+                    make_list(Right, h_w, ri-1)
+                    join_horizontal(temp,Left,Right,val)
+                elif(type(le) != int and type(ri) == int):
+                    make_list(Right, h_w, ri-1)
+                    join_horizontal(temp,temp[int(le[1])],Right,val)
+                elif(type(le) == int and type(ri)!= int):
+                    make_list(Left, h_w, le-1)
+                    join_horizontal(temp,Left,temp[int(ri[1])],val)
+                else:
+                    join_horizontal(temp,temp[int(le[1])],temp[int(ri[1])],val)
                 print(temp)
         
             stack.append('D'+str(val))
             val+=1
 
+        #when the node is a number just append it to the stack
         else:
             stack.append(p_e[i])
 
+    # This part is used to find the final efficient height and width after the traversal of the tree.
     height = 0
     width = 0
     area_min = float("inf")
@@ -102,12 +160,16 @@ def optimal_solution (p_e,h_w):
             height = temp[m-1][k][0]
             width = temp[m -1][k][1]
 
+    
     print("The efficient height and width are  "+str(height)+" X "+str(width))
+    print(expr)
+
+    t_d = [(height,width)]
+    topdown(expr, temp, h_w, t_d)
                    
                    
     
-
-        
+#optimal_solution([1,2,3,'V',4,'V','V'],[(5,2),(5,4),(5,6),(5,7)])
+#optimal_solution([1,2,'H',3,4,'H','V'],[(2,4),(3,4),(2,4),(3,4)])      
 optimal_solution([1,2,3,4,'H','V',5,6,'V','H','V'],
                  [(10,5),(7,2),(4,5),(3,5),(3,3),(3,4)])
-
